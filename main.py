85e02daf2c933113e854951a5edf7e0362bf29ee
Prunel Mel <132164762+PrunelMel@users.noreply.github.com>
2024-06-12 17:43:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ async def get_lecture(date:str = str(datetime.date.today()) , zone:str = "afriqu
     response = requests.get(f"https://api.aelf.org//v1/messes/{date}/{zone}")
 
 
-    return {"status_code":response.status_code, "data":{"date":datetime.date.today(), "ref":response.json()["messes"][0]['lectures'][2]["ref"]}}
+    return {"status_code":response.status_code, "data":{"date":datetime.date.today(), "ref":response.json()["messes"][0]['lectures'][2]["ref"], "text":["messes"][0]['lectures'][2]['contenu']}}
 
 
 @app.get("/lectures")
